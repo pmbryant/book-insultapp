4ffefaf6b2e6ceebb8c8badf8201150246d7935d
Peter Bryant <peterbryant@peters-mbp.attlocal.net>
2018-01-16 14:49:04 -0600

    Updated insult app to use database

--- src/main/java/org/openshift/InsultGenerator.java
+++ src/main/java/org/openshift/InsultGenerator.java
@@ -4,16 +4,37 @@ import java.util.Random;
 
 public class InsultGenerator {
 	public String generateInsult() {
-		String words[][] = {{"Artless", "Bawdy", "Beslubbering"}, {"Base-court", "Bat-fowling", "Beef-witted"}, {"Apple-john", "Baggage", "Barnacle"}};
 		String vowels = "AEIOU";
 		String article = "an";
-		String firstAdjective = words[0][new Random().nextInt(words[0].length)];
-		String secondAdjective = words[1][new Random().nextInt(words[1].length)];
-		String noun = words[2][new Random().nextInt(words[2].length)];
-		if (vowels.indexOf(firstAdjective.charAt(0)) == -1) {
-			article = "a";
+		String theInsult = "";
+		
+		try {
+			String dbURL = "jdbc:postgresql://";
+			dbURL += System.getenv("POSTGRESQL_SERVICE_HOST");
+			dbURL += "/" + System.getenv("POSTGRESQL_SERVICE_PORT");
+			
+			String username = System.getenv("POSTGRESQL_USER");
+			String pwd = System.getenv("PGPASSWORD");
+			
+			Connection conn = DriverManager.getConnection(dbURL, username, pwd);
+			if ( conn != null ) {
+				String sql = "select a.string AS first, b.string AS second, c.string AS noun from short_adjective a, long_adjective b, noun c ORDER BY random() limit 1";
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(sql);
+				while (rs.next()) {
+					if (vowels.indexOf(rs.getString("first").charAt(0)) == -1 ) {
+						article = "a";
+					}
+					theInsult = String.format("Thou art %s %s %s %s!",article,rs.getString("first"),rs.getString("second"),rs.getString("noun"));
+				}
+				rs.close();
+				conn.close();
+			}
+			
+		} catch (Exception e) {
+			return "Database connxn problem!";
 		}
-		return String.format("Thou art %s %s %s %s!", article, firstAdjective, secondAdjective, noun);
+		return theInsult;
 	}
 
 }
